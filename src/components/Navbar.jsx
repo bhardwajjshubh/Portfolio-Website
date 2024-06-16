55b9cdfe0b504fce1879396658e574c799df9576
Shubham Kumar <111330969+bhardwajjshubh@users.noreply.github.com>
2024-06-16 20:24:31 +0530

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -134,7 +134,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/1pjc_VmAjvL97lMVo6LsY0Dp3fsDalrzd/view?usp=drivesdk'  target='_blank' rel="noreferrer"
+              href='https://drive.google.com/file/d/1qQmkm1UIWjECxr-oPsderg__O7R1sM6p/view?usp=drivesdk'  target='_blank' rel="noreferrer"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
