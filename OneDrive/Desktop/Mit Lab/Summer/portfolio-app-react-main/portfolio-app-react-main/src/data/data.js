42af52d1934c411f3c1880dbb923b9cc8c6a698e
Shubham Kumar <shubhambhar99397@gmail.com>
2024-10-20 19:34:13 +0530

    refactered

---
+++ OneDrive/Desktop/Mit Lab/Summer/portfolio-app-react-main/portfolio-app-react-main/src/data/data.js
@@ -0,0 +1,56 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+import Contact from '../assets/projects/contact.png'
+import Music from '../assets/projects/MusicPlayer.png'
+import weather from '../assets/projects/weather.png'
+import parallex from '../assets/projects/parallex.png'
+import chair from '../assets/projects/chair.png'
+
+
+export const data=[
+    {
+        id:1,
+        name:"Hotel Booking",
+        image:RealEst,
+        github:"https://github.com/bhardwajjshubh/endSemProject/tree/main/Project%20End%20sem",
+        live:"https://htmlpreview.github.io/?https://raw.githubusercontent.com/bhardwajjshubh/endSemProject/main/Project%20End%20sem/home.html#hero",
+    },
+    {
+        id:2,
+        name:"Contact-Us Application",
+        image:Contact,
+        github:"https://github.com/bhardwajjshubh/Contact-Us",
+        live:"https://bhardwajjshubh.github.io/Contact-Us/",
+    },
+    {
+        id:3,
+        name:"Music Player",
+        image:Music ,
+        github:"https://github.com/bhardwajjshubh/Music-Player",
+        live:"https://bhardwajjshubh.github.io/Music-Player/",
+    },
+    {
+        id:4,
+        name:"Weather Forecast",
+        image:weather,
+        github:"https://github.com/bhardwajjshubh/Weather-Forecast",
+        live:"https://bhardwajjshubh.github.io/Weather-Forecast/",
+    },
+    {
+        id:5,
+        name:"Parallax website",
+        image:parallex,
+        github:"https://github.com/bhardwajjshubh/Parallax-Website",
+        live:"https://bhardwajjshubh.github.io/Parallax-Website/",
+    },
+    {
+        id:6,
+        name:"Modern Chair",
+        image:chair,
+        github:"https://github.com/bhardwajjshubh/Modern-Chair",
+        live:"https://bhardwajjshubh.github.io/Modern-Chair/",
+    },
+    
+    
+
+]
